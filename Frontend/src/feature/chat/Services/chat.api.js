2996afaf6cc80803d07eb7cf8e1d1d2922e5f6f5
TheShivaji <shivajijagdale2005@gmail.com>
2026-05-01 17:44:57 +0530

    feat(api): implement chat API services with axios

---
+++ Frontend/src/feature/chat/Services/chat.api.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: "http://localhost:5000/api/chat",
+    withCredentials: true
+});
+
+export const sendMessageAPI = async (data) => {
+    try {
+        const response = await api.post("/send", data);
+        return response.data;
+    } catch (error) {
+        console.log("Error in sendMessage api" , error.message)
+        throw error.response?.data?.message || error.message
+    }
+}   
+
+export const getChatsAPI = async () => {
+    try {
+        const response = await api.get("/list");
+        return response.data;
+    } catch (error) {
+        console.log("Error in getChats api" , error.message)
+        throw error.response?.data?.message || error.message
+    }
+}       
+
+export const getMessagesAPI = async (chatId) => {
+    try {
+        const response = await api.get(`/${chatId}/getmessage`);
+        return response.data;
+    } catch (error) {
+        console.log("Error in getMessages api" , error.message)
+        throw error.response?.data?.message || error.message
+    }
+}       
+
+export const deleteChatAPI = async (chatId) => {
+    try {
+        const response = await api.delete(`/${chatId}/deletechat`);
+        return response.data;
+    } catch (error) {
+        console.log("Error in deleteChat api" , error.message)
+        throw error.response?.data?.message || error.message
+    }
+}       
