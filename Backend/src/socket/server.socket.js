e63570ba3b87b13fac6fbed6bdb68079e9b7911b
TheShivaji <shivajijagdale2005@gmail.com>
2026-05-13 13:12:12 +0530

    fix: production socket connection

--- Backend/src/socket/server.socket.js
+++ Backend/src/socket/server.socket.js
@@ -1,26 +1,33 @@
 import { Server } from "socket.io";
 
 let io;
+
 export const initlizeSocket = (server) => {
+
     io = new Server(server, {
         cors: {
-            origin: 'http://localhost:5173',
+            origin: process.env.CLIENT_URL,
             credentials: true
         }
     });
+
     console.log("socket server initialized");
 
     io.on("connection", (socket) => {
         console.log("a user connected", socket.id);
+
+        socket.on("disconnect", () => {
+            console.log("user disconnected", socket.id);
+        });
     });
-}
+};
 
 export const getSocketInstance = () => {
-    if(!io) {
+
+    if (!io) {
         console.error("Socket not initialized");
         return null;
     }
+
     return io;
-}
-
-
+};
--- Frontend/src/feature/chat/Services/chat.socket.js
+++ Frontend/src/feature/chat/Services/chat.socket.js
@@ -1,16 +1,23 @@
 import { io } from "socket.io-client";
 
-
 let socket;
+
 export const initlizeSocket = () => {
-    socket = io("http://localhost:3000");
+
+    socket = io(
+        import.meta.env.VITE_API_URL.replace("/api", ""),
+        {
+            withCredentials: true
+        }
+    );
 
     socket.on("connect", () => {
         console.log("Socket connected");
     });
+
     socket.on("connect_error", (err) => {
         console.error("Connection Error:", err.message);
     });
 
     return socket;
-}
+};
